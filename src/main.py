84c27bace3d706048395f1aebb7884d9c92e09d4
Lord-A2K <amirkiankaveh@gmail.com>
2025-02-23 00:09:02 +0330

    maybe rollback later

--- src/main.py
+++ src/main.py
@@ -25,21 +25,20 @@ async def download_video(payload: DownloadPayload, api_key: str = Depends(get_ap
     file_name = payload.url
     file_name = file_name[file_name.find("v=") :]
     file_name = clean_filename(file_name)
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file_path = os.path.join(temp_dir, f"{file_name}.mp4")
-        ydl_opts = {
-            "format": payload.format,
-            "outtmpl": temp_file_path,
-            "noplaylist": payload.noplaylist,
-            "geo_bypass": payload.geo_bypass,
-        }
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([payload.url])
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Failed to download video: {str(e)}"
-            )
-        return FileResponse(
-            temp_file_path, media_type="video/mp4", filename=f"{file_name}.mp4"
+    temp_file_path = os.path.join(f"{file_name}.mp4")
+    ydl_opts = {
+        "format": payload.format,
+        "outtmpl": temp_file_path,
+        "noplaylist": payload.noplaylist,
+        "geo_bypass": payload.geo_bypass,
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([payload.url])
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to download video: {str(e)}"
         )
+    return FileResponse(
+        temp_file_path, media_type="video/mp4", filename=f"{file_name}.mp4"
+    )
